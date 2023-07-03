213bd34341d9519042b8fa1c83c4152b4ece0b5e
Engkos Kostaman <900021@hq.bni.co.id>
2023-07-03 17:35:08 +0700

    update mvc

--- src/main/java/engkosdeveloper/webmvc/controller/HelloController.java
+++ src/main/java/engkosdeveloper/webmvc/controller/HelloController.java
@@ -1,17 +1,23 @@
 package engkosdeveloper.webmvc.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @Controller
 public class HelloController {
 
     @RequestMapping(path = "/helo")
-    public  void  helloWorld(HttpServletResponse response) throws IOException {
-   response.getWriter().println("Hello Worls");
+    public  void  helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
+   String name = request.getParameter("name");
+   if(Objects.isNull(name)){
+       name="Guest";
+   }
+        response.getWriter().println("Hello " + name);
     }
 
 }
